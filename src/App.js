a9a9d1d07d80de15392ad1d557e436b498e61642
Monet Cooper <smomoc@Monets-MacBook-Pro-3.local>
2020-08-05 00:51:04 -0400

    created Features Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, { Component } from 'react';
 
 // Normalizes string as a slug - a string that is safe to use
 // in both URLs and html attributes
-import slugify from 'slugify';
 
 import './App.css';
 import {Cart} from './Cart';
-
+import {Features} from './Features';
 
 class App extends Component {
   state = {
@@ -39,40 +38,6 @@ class App extends Component {
   };
 
   render() {
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
-              onChange={e => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({new Intl.NumberFormat('en-US', {
-              style: 'currency',
-              currency: 'USD'
-              }).format(item.cost)})
-            </label>
-          </div>
-        );
-      });
-
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });
-
     return (
       <div className="App">
         <header>
@@ -81,7 +46,11 @@ class App extends Component {
         <main>
           <form className="main__form">
             <h2>Customize your laptop</h2>
-            {features}
+           <Features 
+            features={this.props.features} 
+            selected={this.state.selected}
+            onChange={this.updateFeature}
+           />
           </form>
           <Cart selected={this.state.selected} />
         </main>
---
+++ src/Features.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import slugify from 'slugify';
+
+
+export class Features extends React.Component{
+    render(){
+        return(
+            Object.keys(this.props.features).map((feature, idx) => {
+                    const featureHash = feature + '-' + idx;
+                    const options = this.props.features[feature].map(item => {
+                      const itemHash = slugify(JSON.stringify(item));
+                      return (
+                        <div key={itemHash} className="feature__item">
+                          <input
+                            type="radio"
+                            id={itemHash}
+                            className="feature__option"
+                            name={slugify(feature)}
+                            checked={item.name === this.props.selected[feature].name}
+                            onChange={e => this.props.onChange(feature, item)}
+                          />
+                          <label htmlFor={itemHash} className="feature__label">
+                            {item.name} ({new Intl.NumberFormat('en-US', {
+                            style: 'currency',
+                            currency: 'USD'
+                            }).format(item.cost)})
+                          </label>
+                        </div>
+                      );
+                    });
+                    return (
+                      <fieldset className="feature" key={featureHash}>
+                        <legend className="feature__name">
+                          <h3>{feature}</h3>
+                        </legend>
+                        {options}
+                      </fieldset>
+                    );
+                  })
+            )
+        }
+    }
